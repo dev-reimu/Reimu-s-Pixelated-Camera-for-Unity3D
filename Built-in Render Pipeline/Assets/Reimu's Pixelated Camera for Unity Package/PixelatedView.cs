798b06e7d95b3a6403e4dd63e6c8babc43e1dba0
dev-reimu <github.543ov@slmails.com>
2025-05-24 08:43:10 +0300

    Restructure folders + Fixes resolution change at runtime + Inspector resolution change at runtime

--- Built-in Render Pipeline/Assets/Reimu's Pixelated Camera for Unity Package/PixelatedView.cs
+++ Built-in Render Pipeline/Assets/Reimu's Pixelated Camera for Unity Package/PixelatedView.cs
@@ -4,21 +4,18 @@ public class PixelatedView : MonoBehaviour
 {
     RenderTexture renderTexture;
 
-    [SerializeField]
-    [Header("(Hover over this variable for a notice)")]
-    [Tooltip("This variable can be edited during runtime only through scripts, and does not support changes from the Inspector.")]
-    short _desiredPixelDensity = 128;
-
+    [SerializeField] short _desiredPixelDensity = 128;
     public short desiredPixelDensity
     {
         get { return _desiredPixelDensity; }
         set
         {
             _desiredPixelDensity = value;
+
+            renderTexture.Release();
             renderTexture.height = value;
             renderTexture.width = (int)((float)Screen.currentResolution.width / (float)Screen.currentResolution.height * value);
-
-            renderTexture.ApplyDynamicScale();
+            renderTexture.Create();
         }
     }
 
@@ -27,4 +24,8 @@ public class PixelatedView : MonoBehaviour
         renderTexture = GetComponent<Camera>().targetTexture;
         desiredPixelDensity = _desiredPixelDensity;
     }
+
+    #if UNITY_EDITOR
+    void Update() => desiredPixelDensity = _desiredPixelDensity;
+    #endif
 }
